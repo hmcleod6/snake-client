e7621a313198a8857d1763610e6b7a794d73349d
Heather <hmcleod29@gmail.com>
2022-05-11 13:36:22 -0400

    Following first steps of connecting the client to the server

---
+++ client.js
@@ -0,0 +1,18 @@
+let connect = require('./connect')
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
